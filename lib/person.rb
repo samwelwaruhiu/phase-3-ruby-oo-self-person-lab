90956997f12283d7847a44c64f5eaab9c4282e5e
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-10-23 20:55:01 +0300

    complited

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,90 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+
+    end
+
+    def happiness= (value)
+        @happiness = 
+        if value > 10
+            10
+        elsif value < 0
+            0
+        else
+            value
+        end
+    end
+
+
+    def hygiene= (value)
+        @hygiene = 
+        if value > 10
+            10
+        elsif value < 0
+            0
+        else
+            value
+        end
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+        
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            friend.happiness -= 2
+            self.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            friend.happiness += 1
+            self.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+     
+    end
+
+ 
+
+
+end
+
